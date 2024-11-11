c2b991ade32b9bb6fd9405fc97545019f828afaa
Nathan Němec <nathan.nemec.cz@gmail.com>
2024-11-11 02:06:22 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# sc2ai-getting-started
+# SC2AI getting started
+
+## APIs
+- NodeJs:
+    - [node-sc2](https://github.com/node-sc2/core/tree/wip_0.1.0)
+    - [s2client-api-typescript](https://github.com/Zamiell/s2client-api-typescript) (not tested)
+- Python:
+    - [python-sc2](https://github.com/BurnySc2/python-sc2)
+- Rust:
+    - [rust-sc2](https://github.com/UltraMachine/rust-sc2) (not tested)
+- Go:
+    - [gp-sc2ai](https://github.com/chippydip/go-sc2ai) (not tested)
+- Java:
+    - [ocraft-s2client](https://github.com/ocraft/ocraft-s2client) (not tested)
+- C++:
+    - [spp-sc2](https://github.com/cpp-sc2/cpp-sc2)
+
+## Tutorials
+TODO
+
+## Examples
+- [node](/examples/node/)
+- [python](/examples/python/)
+
+Another awesome list: https://github.com/aiarena/awesome-sc2-ai
+
+Big UP to https://github.com/aiarena
---
+++ examples/node/src/index.js
@@ -0,0 +1,57 @@
+const { createAgent, createEngine, createPlayer } = require('@node-sc2/core');
+const { Difficulty, Race, PlayerType } = require('@node-sc2/core/constants/enums');
+const { COMMANDCENTER, SCV } = require('@node-sc2/core/constants/unit-type');
+
+const bot = createAgent({
+    settings: {
+        race: Race.TERRAN
+    },
+    async onGameStart({ resources }) {
+        console.log('start')
+        const { units, actions, map } = resources.get();
+
+        const workers = units.getWorkers();
+
+        console.log('end')
+        return actions.gather(workers)
+        //return actions.attackMove(workers, map.getEnemyMain().townhallPosition);
+    },
+    async onStep({ agent, data, resources }) {
+        const { units, actions } = resources.get();
+        //console.log(units)
+        const myBase = units.getById(COMMANDCENTER);
+        console.log(myBase);
+        try {
+            actions.train(SCV);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+});
+
+const engine = createEngine({
+    host: "127.0.0.1",
+    port: "8083",
+    launch: false
+});
+
+engine.connect().then(async (ping) => {
+    console.log(ping)
+
+    let response = await engine.createGame('Equilibrium513AIE.SC2Map', [
+        createPlayer({
+            race: Race.TERRAN,
+            type: PlayerType.PARTICIPANT
+        }, bot),
+        createPlayer({
+            race: Race.RANDOM,
+            difficulty: Difficulty.EASY,
+        })
+    ], true);
+
+    console.log(response)
+
+    console.log(await engine.joinGame(bot))
+
+    console.log("done")
+});
